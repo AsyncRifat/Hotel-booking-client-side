c364ab4358b07bc9b5fbb738de484886c7058983
AsyncRifat <ibrahim3rifat@gmail.com>
2025-06-24 11:06:45 +0600

    Booking add in Database

--- src/pages/rooms/roomDetails/RoomDetails.jsx
+++ src/pages/rooms/roomDetails/RoomDetails.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import { Link, useLoaderData } from 'react-router';
+import React, { useState } from 'react';
+import { Link, useLoaderData, useNavigate } from 'react-router';
 import { MdDescription } from 'react-icons/md';
 import { IoLocationOutline } from 'react-icons/io5';
 import useDocumentTitle from '../../../hooks/useDocumentTitle';
 import { AiOutlineRollback } from 'react-icons/ai';
 import { FaStar } from 'react-icons/fa6';
+import Swal from 'sweetalert2';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const RoomDetails = () => {
-  useDocumentTitle('Room Details ');
+  useDocumentTitle('Room Details');
   const singleRoomDetails = useLoaderData();
+  const navigate = useNavigate();
 
   const {
     title,
@@ -26,10 +30,70 @@ const RoomDetails = () => {
     available,
     capacity,
   } = singleRoomDetails;
+
+  const [confirmOrder, setConfirmOrder] = useState(false);
+
+  const [startedDate, setStatedDate] = useState('');
+  const [endedDate, setEndedDate] = useState('');
+  const handleConfirm = e => {
+    e.preventDefault();
+
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+
+    if (!startedDate || !name || !email || !endedDate) {
+      toast.warn('Please fill-up all information!');
+      return;
+    }
+    if (!available) {
+      toast.warn('Sorry sir! Room not available now');
+      return;
+    }
+
+    const orderData = {
+      roomId: _id,
+      title,
+      description,
+      price,
+      name: name,
+      email: email,
+      statedDate: startedDate,
+      EndedDate: endedDate,
+      photo: photo.photo1,
+    };
+
+    console.log(orderData);
+    // Call parent onConfirm handler
+    // onConfirm(orderData);
+
+    axios
+      .post(`${import.meta.env.VITE_API_URL}/order`, orderData)
+      .then(res => {
+        if (res.data?.insertedId) {
+          Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Your application has been submitted',
+            showConfirmButton: false,
+            timer: 1500,
+            width: '300px',
+          });
+        }
+        e.target.reset();
+        navigate('/my-booking');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-12 gap-5 my-5">
-        <div className="md:col-span-8 border border-gray-300 dark:border-gray-600 rounded-2xl p-5">
+        {confirmOrder && (
+          <div className="fixed inset-0 bg-black/5 backdrop-blur-xs z-10"></div>
+        )}
+        <div className="relative md:col-span-8 border border-gray-300 dark:border-gray-600 rounded-2xl p-5">
           <div className="flex justify-between items-center">
             <div>
               <p className="text-xl md:text-4xl font-bold mb-2 openSans">
@@ -43,12 +107,103 @@ const RoomDetails = () => {
               </div>
             </div>
 
-            <Link to={`/jobApply/${_id}`}>
-              <button className="btn bg-blue-500 hover:bg-blue-600 duration-700 ease-in-out hover:scale-105 transition-all text-white">
+            <Link onClick={() => setConfirmOrder(true)}>
+              <button className=" btn bg-blue-500 hover:bg-blue-600 duration-700 ease-in-out hover:scale-105 transition-all text-white">
                 Booking Now
               </button>
             </Link>
           </div>
+
+          {confirmOrder && (
+            <div className="absolute top-10 right-0 md:w-[450px] bg-base-100 dark:bg-gray-800 border border-blue-300 dark:border-gray-600 shadow-xl rounded-xl p-5 z-20">
+              <div className="flex justify-between items-center mb-2">
+                <h2 className="text-lg font-bold text-gray-800 dark:text-white">
+                  {title}
+                </h2>
+                <button
+                  onClick={() => setConfirmOrder(false)}
+                  className="text-red-500 text-xl font-bold"
+                >
+                  ✕
+                </button>
+              </div>
+              <p className="text-gray-800 dark:text-white text-sm">
+                {description}
+              </p>
+              <img
+                src={photo.photo1}
+                alt=""
+                className="w-full h-[200px] object-cover rounded-xl my-4"
+              />
+              <p className=" gap-x-2 mb-3">
+                <span className=" text-gray-500 gap-1.5">Price:</span>${price}{' '}
+                <span className="font-light text-xs">Every Night</span>
+              </p>
+
+              <form onSubmit={handleConfirm}>
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text">Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Enter Your Name"
+                    className="input input-bordered w-full"
+                  />
+                </div>
+
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    className="input input-bordered w-full"
+                  />
+                </div>
+
+                <div className="flex w-full gap-x-3">
+                  <div className="flex-1 mb-4 ">
+                    <label
+                      htmlFor="date"
+                      className="text-gray-600 dark:text-gray-400 block mb-1"
+                    >
+                      Select Date:
+                    </label>
+                    <input
+                      type="date"
+                      id="date"
+                      value={startedDate}
+                      onChange={e => setStatedDate(e.target.value)}
+                      className="w-full border border-gray-400 rounded px-3 py-2"
+                    />
+                  </div>
+                  <div className="flex-1 mb-4">
+                    <label
+                      htmlFor="date"
+                      className="text-gray-600 dark:text-gray-400 block mb-1"
+                    >
+                      Select Date:
+                    </label>
+                    <input
+                      type="date"
+                      id="date"
+                      value={endedDate}
+                      onChange={e => setEndedDate(e.target.value)}
+                      className="w-full border border-gray-400 rounded px-3 py-2"
+                    />
+                  </div>
+                </div>
+
+                <button type="submit" className="btn btn-success mt-5">
+                  Confirm Order
+                </button>
+              </form>
+            </div>
+          )}
 
           <hr className="mt-4 dark:text-gray-700 text-gray-200" />
 
@@ -84,22 +239,18 @@ const RoomDetails = () => {
             <hr className="m-3 dark:text-gray-700 text-gray-200" />
             <div className="grid grid-cols-1 md:grid-cols-8 px-3.5">
               <div className="mt-3 col-span-4">
-                <p className="flex items-center gap-x-2 mb-6">
-                  <span className=" flex items-center text-gray-500 gap-1.5">
-                    Bed Type:
-                  </span>
+                <p className=" gap-x-2 mb-6">
+                  <span className="  text-gray-500 gap-1.5">Bed Type: </span>
                   {bedType}
                 </p>
 
-                <p className="flex items-center gap-x-2 mb-4">
-                  <span className=" flex items-center text-gray-500 gap-1.5">
-                    Capacity:
-                  </span>
+                <p className=" gap-x-2 mb-4">
+                  <span className=" text-gray-500 gap-1.5">Capacity: </span>
                   {capacity}
                 </p>
 
                 <div className="">
-                  <p className=" text-gray-500 gap-x-1.5">Amenities:</p>
+                  <p className=" text-gray-500 gap-x-1.5">Amenities: </p>
                   <div className="pt-3">
                     {amenities.map((Facilities, index) => (
                       <span
@@ -113,27 +264,20 @@ const RoomDetails = () => {
                 </div>
               </div>
               <div className="justify-start col-span-4 mt-3">
-                <p className="flex items-center gap-x-2 mb-6">
-                  <span className=" flex items-center text-gray-500 gap-1.5">
-                    Price:
-                  </span>
-                  ${price}{' '}
+                <p className=" gap-x-2 mb-6">
+                  <span className=" text-gray-500 gap-1.5">Price: </span>${price}{' '}
                   <span className="font-light text-xs">Every Night</span>
                 </p>
 
-                <p className="flex items-center gap-x-2 mb-6">
-                  <span className=" flex items-center text-gray-500 gap-1.5">
-                    Available:
-                  </span>
+                <p className=" gap-x-2 mb-6">
+                  <span className=" text-gray-500 gap-1.5">Available: </span>
                   <span className="uppercase font-semibold text-green-600">
                     {available}
                   </span>
                 </p>
 
-                <p className="flex items-center gap-x-2 mb-6">
-                  <span className=" flex items-center text-gray-500 gap-1.5">
-                    Room Size:
-                  </span>
+                <p className="gap-x-2 mb-6">
+                  <span className=" text-gray-500 gap-1.5">Room Size: </span>
                   {roomSize} Square feet
                 </p>
               </div>
