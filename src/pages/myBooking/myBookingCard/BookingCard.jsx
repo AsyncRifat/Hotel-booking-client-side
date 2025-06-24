1cac8cbe576c1b6a10badebaacd2a4e4615fe783
AsyncRifat <ibrahim3rifat@gmail.com>
2025-06-24 19:49:25 +0600

    booking cencel  method done

--- src/pages/myBooking/myBookingCard/BookingCard.jsx
+++ src/pages/myBooking/myBookingCard/BookingCard.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
 
 const BookingCard = ({ singleBooking }) => {
-  const { title, photo, price, statedDate, EndedDate } = singleBooking;
+  const [myBooking, setMyBooking] = useState(null);
+
+  // console.log(_id);
   // console.log(photo);
-  console.log(singleBooking);
+
+  // console.log(singleBooking);
+
+  useEffect(() => {
+    if (singleBooking) {
+      setMyBooking(singleBooking);
+    }
+  }, [singleBooking]);
+
+  const handleDelete = id => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then(result => {
+      if (result.isConfirmed) {
+        fetch(`${import.meta.env.VITE_API_URL}/booking-delete/${id}`, {
+          method: 'DELETE',
+        })
+          .then(res => res.json())
+          .then(data => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: 'Deleted!',
+                text: 'Your Room has been deleted.',
+                icon: 'success',
+              });
+              setMyBooking(null);
+            }
+          });
+      }
+    });
+  };
+  if (!myBooking) return null;
+  const { title, photo, price, statedDate, EndedDate, _id } = myBooking;
+
   return (
     <>
       <div className="card card-side bg-base-100 dark:bg-gray-800 shadow-md">
@@ -33,7 +75,12 @@ const BookingCard = ({ singleBooking }) => {
 
           <div className="card-actions justify-end">
             <button className="btn btn-info">Review</button>
-            <button className="btn btn-outline btn-error">Cancel</button>
+            <button
+              onClick={() => handleDelete(_id)}
+              className="btn btn-outline btn-error"
+            >
+              Cancel
+            </button>
           </div>
         </div>
       </div>
